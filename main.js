69040b69f73ef9a2a4a5f414d5fe03ebd430bf51
lilkahk <kahkeshan.ryan@gmail.com>
2023-09-19 00:26:23 -0400

    Added getCurrentWeather to retrieve the current weather for any location from the API

---
+++ main.js
@@ -0,0 +1,2 @@
+!async function(a){const o=await fetch("http://api.weatherapi.com/v1/current.json?key=202d4f2a7784455d9f5183029231809&q=waterloo&days=3&aqi=no&alerts=no"),t=await o.json();console.log(t)}();
+//# sourceMappingURL=main.js.map
